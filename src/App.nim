8b7b12e15cc09e7ec7c96e7660e1de8b83eeb9fa
Marco Mengelkoch <Mmengelkoch@gmx.de>
2022-02-17 00:21:32 +0100

    added wait

--- src/App.nim
+++ src/App.nim
@@ -4,7 +4,7 @@ import os
 nimview.add("spawnShell", proc (cmd: string): int =
   when defined(windows):
     echo "starting " & os.quoteShellPosix(cmd)
-    os.execShellCmd("start cmd.exe /c bash -c " & os.quoteShellPosix(cmd))
+    os.execShellCmd("start /wait cmd.exe /c bash -c " & os.quoteShellPosix(cmd))
   else:
     os.execShellCmd("xterm -e /usr/bin/bash -c " & os.quoteShellPosix(cmd))
 )
