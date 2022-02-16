6aba6ee23ca4ef61378b4f6fa39f2aa76c5976a8
Marco Mengelkoch <Mmengelkoch@gmx.de>
2022-02-16 23:48:56 +0100

    added sh call

--- src/App.nim
+++ src/App.nim
@@ -1,4 +1,12 @@
 import nimview
+import os
+
+nimview.add("spawnShell", proc (cmd: string): int =
+  when defined(windows):
+    os.execShellCmd("start cmd.exe /c wsl.exe \"" & os.quoteShellPosix(cmd) & "\"")
+  else:
+    os.execShellCmd("xterm -e /bin/bash -c \"" & os.quoteShellPosix(cmd) & "\"")
+)
 
 when isMainModule:
   enableStorage() # adds getStoredVal and setStoredVal
