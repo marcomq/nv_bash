f0b73f226c41a9465e86f8841b6e81f17533f7eb
Marco Mengelkoch <Mmengelkoch@gmx.de>
2022-02-19 20:01:15 +0100

    improved window handling

--- src/App.nim
+++ src/App.nim
@@ -1,12 +1,21 @@
 import nimview
-import os
+import os, strutils
+import std/compilesettings
 
+if defined(gui):
+  quit()
 nimview.add("spawnShell", proc (cmd: string): int =
   when defined(windows):
-    
-    let winCmd =  "start /wait cmd.exe /c bash -c " & 
+    let compileCmd: string = querySetting(SingleValueSetting.commandLine)
+    echo compileCmd
+    let showConsole = 
+      if "app:gui" in compileCmd:
+        " /b "
+      else:
+        ""
+    let winCmd =  "start " & showConsole & " /wait cmd.exe /c bash -c " & 
       os. quoteShellWindows(cmd & " && sleep 5 || read -n 1 -p \"press any key\" VAR")
-    echo "starting " & winCmd
+    echo "starting: " & winCmd
     os.execShellCmd(winCmd)
   else:
     let posixCmd =  os.quoteShellPosix(cmd & " && sleep 5 || read -n 1 -p \"press any key\" VAR")
