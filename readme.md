c74f9628bbaa5312d6ce3318d3b6cb319efac7b5
Marco Mengelkoch <Mmengelkoch@gmx.de>
2022-02-19 14:41:58 +0100

    fixes & readme

--- readme.md
+++ readme.md
@@ -1,7 +1,15 @@
 # nimview Svelte Todo-list Application
 
 This is the source code for the demo app on the Nimview page.
-To compile your code, just run
+To prepare the build, install [nim](https://nim-lang.org/install.html) >= 1.4.8 and run
+- nimble install nimview
 - npm install
+
+Then to build a release 
 - npm run build
-- nim c -r --app:gui src/App.nim
+- nim c -r --app:gui -d:release src/App.nim
+
+Or during development
+- npm run dev
+- nim c -r -d:debug src/App.nim
+- open browser 
--- src/App.nim
+++ src/App.nim
@@ -3,10 +3,13 @@ import os
 
 nimview.add("spawnShell", proc (cmd: string): int =
   when defined(windows):
-    echo "starting " & os.quoteShellPosix(cmd)
-    os.execShellCmd("start /wait cmd.exe /c bash -c " & os.quoteShellPosix(cmd))
+    let winCmd =  "start /wait cmd.exe /c bash -c " & 
+      os. quoteShellWindows(cmd & " && sleep 5 || read -n 1 -p \"press any key\" VAR")
+    echo "starting " & winCmd
+    os.execShellCmd(winCmd)
   else:
-    os.execShellCmd("xterm -e /usr/bin/bash -c " & os.quoteShellPosix(cmd))
+    let posixCmd =  os.quoteShellPosix(cmd & " && sleep 5 || read -n 1 -p \"press any key\" VAR")
+    os.execShellCmd("xterm -e /usr/bin/bash -c " & posixCmd)
 )
 
 when isMainModule:
